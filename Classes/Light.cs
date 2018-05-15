3b608587ecce72a415cb6b8969b8ec1e35ead4e6
Amber Elferink <a.s.elferink@students.uu.nl>
2018-05-15 13:46:01 +0200

    Map in solution explorer geplaatst zodat vector ed werken

--- Classes/Light.cs
+++ Classes/Light.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿
+using OpenTK;
 
-public class Light
-{
-	public Light()
-	{
-        Vector3 location = new Vector3(0, -15, 5); //5 behind and 5 above the camera
-        Vector3 color = new Vector3(255, 255, 255);
-	}
-}
+    public class Light
+    {
+
+        public Light()
+        {
+            Vector3 location = new Vector3(0, -15, 5); //5 behind and 5 above the camera
+            Vector3 color = new Vector3(255, 255, 255);
+
+        }
+    }
---
+++ Classes/Scene.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Scene
+{
+    List<Light> Lights = new List<Light>;
+    
+	public Scene()
+	{
+        
+	}
+}
