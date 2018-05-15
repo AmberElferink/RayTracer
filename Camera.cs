babffb611bbcda850248538501b13370edf70f8c
Miriam <m.f.sterl@uu.nl>
2018-05-15 12:34:59 +0200

    class 'Camera' toegevoegd
    alle membervariabelen zijn nu in de constructormethode vastgezet, straks moeten we dit nog flexibeler maken, zodat we willekeurige posities/richtingen kunnen maken.

---
+++ Camera.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+class Camera
+{
+    // member variables
+    public Vector3 E; // camera position
+    public Vector3 V; // view direction; must be normalized
+    //public double a; // field of view-angle in degrees // convert fov to radians: multiply by Math.PI/180
+    public Vector3 C; // center of the screen 
+    public Vector3 p0; // screen corners
+    public Vector3 p1;
+    public Vector3 p2;
+    // later: make screen corners 
+
+    public Camera()
+    {
+        this.E = (0, 0, -10);
+        this.V = (0, 0, 1);
+        this.C = E + 1 * V; // later: maak van 1 d, berekenen mbv field of view-angle
+        this.p0 = C + (-1, -1, 0);
+        this.p1 = C + (1, -1, 0);
+        this.p2 = C + (-1, 1, 0);
+    }
+}
