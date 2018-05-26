d7a93c652be344e7e97570e4286287cdf091796e
Amber Elferink <a.s.elferink@students.uu.nl>
2018-05-26 18:26:58 +0200

    anti aliasing ingebouwd

--- Classes/Raytracer.cs
+++ Classes/Raytracer.cs
@@ -32,32 +32,54 @@ namespace Template
             {
                 for (int x = 0; x < RscreenWidth; x++)
                 {
-                    Vector3 D = (float)x / (float)RscreenWidth* (camera.p1 - camera.p0) + (float)y / (float)screen.height * (camera.p2 - camera.p0) + camera.p0 - camera.E;
-                    D.Normalize();
                     int raynumber = 0;
-                    Ray ray = new Ray(camera.E, D, 1E30f);
-                    Intersection intersection = scene.Intersect(ray);
-                    screen.pixels[x + y * screen.width] = CreateColor(scene.Trace(ray, intersection, debug, raynumber));
-                    
-                    //Debug output
-                    if (y == screen.height / 2)
-                    {
-                        
-                        raycounter++;
-                        if(raycounter >= 15)
-                        {
-                            if (intersection.prim is Sphere)
-                            {
-                                    debug.DrawRay(camera.E, intersection.point, raynumber);
-                            }
-                            else
-                            {
-                                debug.DrawRay(camera.E, ray.D * ray.t, raynumber);
-                            }
-                            raycounter = 0;
-                        }
 
-                    }
+                     Ray ray1 = new Ray(camera.E, D(x - 0.5f, y + 0.5f), 1E30f);
+                     Ray ray2 = new Ray(camera.E, D(x - 0.5f, y - 0.5f), 1E30f);
+                     Ray ray3 = new Ray(camera.E, D(x + 0.5f, y + 0.5f), 1E30f);
+                     Ray ray4 = new Ray(camera.E, D(x + 0.5f, y - 0.5f), 1E30f);
+                     Ray ray5 = new Ray(camera.E, D(x, y), 1E30f);
+
+                    //Ray ray = new Ray(camera.E, D, 1E30f);
+                    Intersection intersection1 = scene.Intersect(ray1);
+                    Intersection intersection2 = scene.Intersect(ray2);
+                    Intersection intersection3 = scene.Intersect(ray3);
+                    Intersection intersection4 = scene.Intersect(ray4);
+                    Intersection intersection5 = scene.Intersect(ray5);
+
+                    Vector3 color1 = scene.Trace(ray1, intersection1, debug, raynumber);
+                    Vector3 color2 = scene.Trace(ray2, intersection2, debug, raynumber);
+                    Vector3 color3 = scene.Trace(ray3, intersection3, debug, raynumber);
+                    Vector3 color4 = scene.Trace(ray4, intersection4, debug, raynumber);
+                    Vector3 color5 = scene.Trace(ray5, intersection4, debug, raynumber);
+
+                    Vector3 AverageColor; 
+                    AverageColor.X = (color1.X + color2.X + color3.X + color4.X + color5.X) / 5;
+                    AverageColor.Y = (color1.Y + color2.Y + color3.Y + color4.Y + color5.Y) / 5;
+                    AverageColor.Z = (color1.Z + color2.Z + color3.Z + color4.Z + color5.Z) / 5;
+
+                    screen.pixels[x + y * screen.width] = CreateColor(AverageColor);
+
+                   
+                    //Debug output
+                     if (y == screen.height / 2)
+                     {
+
+                         raycounter++;
+                         if(raycounter >= 15)
+                         {
+                             if (intersection5.prim is Sphere)
+                             {
+                                     debug.DrawRay(camera.E, intersection5.point, raynumber);
+                             }
+                             else
+                             {
+                                 debug.DrawRay(camera.E, ray5.D * ray5.t, raynumber);
+                             }
+                             raycounter = 0;
+                         }
+
+                     }
 
                 }
             }
@@ -69,6 +91,13 @@ namespace Template
             int g = (int)(Math.Min(1, color.Y) * 255);
             int b = (int)(Math.Min(1, color.Z) * 255);
             return (r << 16) + (g << 8) + b;
+        }
+
+        Vector3 D(float x, float y)
+        {
+            Vector3 D = (float)x / (float)RscreenWidth * (camera.p1 - camera.p0) + (float)y / (float)screen.height * (camera.p2 - camera.p0) + camera.p0 - camera.E;
+            D.Normalize();
+            return D;
         }
     } //class raytracer
 } //namespace template
